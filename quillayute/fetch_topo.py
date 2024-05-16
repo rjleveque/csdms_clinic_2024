30cfd67588eb6f817a304a3211ae271d59f8b1d7
Randy LeVeque <rjl@uw.edu>
2024-05-15 22:40:53 -0400

    moved repo to https://github.com/clawpack/geoclaw_tutorial_csdms2024

--- quillayute/fetch_topo.py
+++
@@ -1,7 +0,0 @@
-
-from clawpack.clawutil.data import get_remote_file
-
-topo_fname = 'Quillayute_13s.asc'
-url = 'http://depts.washington.edu/clawpack/geoclaw/topo/WA/' + topo_fname
-
-get_remote_file(url, output_dir='.', file_name=topo_fname, verbose=True)
